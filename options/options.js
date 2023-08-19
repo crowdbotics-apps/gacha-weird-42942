18277f96946fcf9261a172ff039e5b3b2d3d353d
Crowdbotics <team@crowdbotics.com>
2023-08-19 06:19:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gacha-weird-42942",
+  url: "https://gacha-weird-42942.botics.co",
+  api: "https://gacha-weird-42942.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
